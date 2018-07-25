62ca681da1d74412c540df6a2bb7d019e7a09c12
Dani Lyn <dlyn1110@gmail.com>
2018-07-25 15:54:04 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,7 @@
 class Dog
 
   attr_accessor :name, :breed, :age
-   @@all = []
+   #@@all = []
 
   def initialize(name, breed, age)
     @name = name
@@ -9,4 +9,7 @@ class Dog
     @age = age
     @@all << self
   end
+
+  def self.all
+    @@all
 end
